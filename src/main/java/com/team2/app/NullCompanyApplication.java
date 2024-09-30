c196fb1c76cf5180cb08847df513e3476c5b4edc
kwan <kwanda1227@gmail.com>
2024-09-30 09:53:00 +0900

    test

---
+++ src/main/java/com/team2/app/NullCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.team2.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2/app/NullCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
