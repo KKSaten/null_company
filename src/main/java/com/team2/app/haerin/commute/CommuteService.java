df1585323c918523720cdfb21eb72a208727a9fc
kimhaerinring <rlagofls0824@naver.com>
2024-10-04 15:49:00 +0900

    출퇴근 사원 리스트

--- src/main/java/com/team2/app/haerin/commute/CommuteService.java
+++ src/main/java/com/team2/app/haerin/commute/CommuteService.java
@@ -1,5 +1,20 @@
 package com.team2.app.haerin.commute;
 
-public class CommuteService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class CommuteService {
+	
+		@Autowired
+		private CommuteMapper commuteMapper;
+	
+		public List<CommuteVO> checkList( ) throws Exception {
+			return commuteMapper.checkList();
+		}
+	
 }
