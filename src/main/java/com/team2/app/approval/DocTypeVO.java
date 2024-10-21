b5c5d920c3e585d1ae93ed9894adf4e08c5fcc31
LeeSiwoo <cocoar147@naver.com>
2024-10-21 17:30:00 +0900

    문서 유형, 템플릿 선택 모달창 작업중

---
+++ src/main/java/com/team2/app/approval/DocTypeVO.java
@@ -0,0 +1,13 @@
+package com.team2.app.approval;
+
+import lombok.Data;
+
+@Data
+public class DocTypeVO {
+	
+	private Integer docTypecode;
+	private String docTypename;
+	private Long docTemplatecode;
+	private String templateName;
+
+}
